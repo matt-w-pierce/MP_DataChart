484830ecb2cf8d8c6383dc5a15699a94943c646f
Matt <matt.pierce5695@gmail.com>
2017-10-03 23:55:45 -0400

    The first page successfully displays all created dataset connections. When selected they are added to a list that can then be passed onto the next page.

--- app/src/main/java/com/mattpierce/mwp_datachart/activities/DataSelectActivity.java
+++ app/src/main/java/com/mattpierce/mwp_datachart/activities/DataSelectActivity.java
@@ -4,19 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.mattpierce.mwp_datachart.R;
 import com.mattpierce.mwp_datachart.objects.DatasetConnection;
+import com.mattpierce.mwp_datachart.objects.DatasetMapping;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class DataSelectActivity extends AppCompatActivity {
 
     private int numConnections;
     private ArrayList<DatasetConnection> connections = new ArrayList<>();
     private DatasetListAdapter adapter;
+    private List<DatasetConnection> selected_connections = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +37,39 @@ public class DataSelectActivity extends AppCompatActivity {
         // When datasets are selected, add them to a list to pass to graph activity
         // When "Next"is clicked, pass onto the graph activity
 
-        // TODO: Write initiailizeView function. Should focus on setting up ListView
+        adapter = new DatasetListAdapter();
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final DatasetConnection connection = adapter.getConnection(i);
+                if (connection == null) return;
+
+                CheckBox checkBox = (CheckBox) view.findViewById(R.id.dataset_checkBox);
+                if (checkBox.isChecked()) {
+                    // Uncheck the box and remove the device from the array
+                    checkBox.setChecked(false);
+                    selected_connections.remove(connection);
+                } else {
+                    // Check the box and add the device to the array
+                    checkBox.setChecked(true);
+                    selected_connections.add(connection);
+                }
+
+                /*
+                final Intent intent = new Intent(MainActivity.this, DeviceDetailActivity.class);
+                intent.putExtra(getResources().getString(R.string.EXTRAS_DEVICE_NAME), device.getName());
+                intent.putExtra(getResources().getString(R.string.EXTRAS_DEVICE_ADDRESS), device.getAddress());
+                if (isScanning) {
+                    scanLeDevice(false);
+                }
+                startActivity(intent);
+                */
+            }
+        });
+
+        // TODO: Write initializeView function. Should focus on setting up ListView
         initializeView();
 
         // TODO: Write connectDatasets function. Should include createDataMappings
@@ -39,6 +79,19 @@ public class DataSelectActivity extends AppCompatActivity {
 
     private int connectDatasets() {
         // This is where you can create all of your datasets
+        // TODO: First, create the data mappings
+        DatasetMapping datasetMapping = new DatasetMapping("xValue", "yValue");
+
+        String conStr1 = "www.helloWorld.com";
+        DatasetConnection datasetConnection1 = new DatasetConnection(datasetMapping, conStr1, DatasetConnection.RAW_JSON_CONNECTION);
+
+        String conStr2 = "www.whatisthis.com";
+        DatasetConnection datasetConnection2 = new DatasetConnection(datasetMapping, conStr2, DatasetConnection.API_JSON_CONNECTION);
+
+        connections.add(datasetConnection1);
+        connections.add(datasetConnection2);
+
+        return connections.size();
     }
 
     private void initializeView() {
@@ -50,38 +103,29 @@ public class DataSelectActivity extends AppCompatActivity {
     private class DatasetListAdapter extends ArrayAdapter<DatasetConnection> {
         DatasetListAdapter()
         {
-            super(DataSelectActivity.this, R.layout.cell_main_device, connections);
+            super(DataSelectActivity.this, R.layout.cell_main_dataset, connections);
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             View itemView = convertView;
             if (itemView == null) {
-                itemView = getLayoutInflater().inflate(R.layout.cell_main_device, parent, false);
+                itemView = getLayoutInflater().inflate(R.layout.cell_main_dataset, parent, false);
             }
 
-            /*
-            // Find the object to work with
-            BluetoothDevice currentDevice = devices.get(position);
+            DatasetConnection currentConn = connections.get(position);
 
-            TextView nameText = (TextView) itemView.findViewById(R.id.deviceName);
-            final String deviceName = currentDevice.getName();
-            if (deviceName != null && deviceName.length() > 0)
-                nameText.setText(deviceName);
-            else
-                nameText.setText(R.string.unknown_device);
+            TextView connStrText = (TextView) itemView.findViewById(R.id.connectionString);
+            TextView connTypeText = (TextView) itemView.findViewById(R.id.connectionType);
 
-            TextView addressText = (TextView) itemView.findViewById(R.id.deviceAddress);
-            addressText.setText(currentDevice.getAddress());
+            final String connStr = currentConn.getJsonDataSource();
+            final String connType = Integer.toString(currentConn.getConnectionType());
 
-            // CheckBox box = (CheckBox) itemView.findViewById(R.id.device_checkBox);
-            // box.setClickable(false);
-            // box.setChecked(false);
-            */
+            connStrText.setText(connStr);
+            connTypeText.setText(connType);
 
             return itemView;
         }
-
-        // BluetoothDevice getDevice(int position) { return devices.get(position); }
+        DatasetConnection getConnection(int position) { return connections.get(position); }
     }
 }
